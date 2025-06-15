f1c4b5214a1322d18051913d438a961997ed0cdd
egemenkopuz <egemen.kopuz.sec@gmail.com>
2025-06-15 20:50:25 +0200

    fix: add missing hls

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -38,14 +38,24 @@ return {
             transparent = vim.g.transparent,
             highlights = function(_)
                 local out = require("user.highlights").general
+                local colors = require "user.colors"
                 out = vim.tbl_deep_extend("force", out, require("user.highlights").dim)
                 if vim.g.transparent then
                     out = vim.tbl_deep_extend("force", out, require("user.highlights").transparent)
                 else
-                    out["Normal"] = { bg = "#19191a" }
-                    out["NormalNC"] = { bg = "#19191a" }
-                    out["NormalFloat"] = { bg = "#19191a" }
+                    local bg_color = "#19191a"
+                    -- stylua: ignore start
+                    out["Normal"] = { bg = bg_color }
+                    out["NormalNC"] = { bg = bg_color }
+                    out["NormalFloat"] = { bg = bg_color }
+                    out["StatusLine"] = { bg = bg_color }
+                    out["MiniTablineCurrent"] = { fg = colors.custom.gray3, bg = bg_color, bold = true }
+                    out["MiniTablineHidden"] = { fg = colors.custom.gray2, bg = bg_color }
+                    out["MiniTablineVisible"] = { fg = colors.custom.gray2, bg = bg_color }
+                    out["StatusLineBackground"] = { bg = bg_color }
+                    -- stylua: ignore end
                 end
+                out["TreesitterContextLineNumber"] = { fg = "#4f4f4f", bg = "#19191a" }
                 out["@variable.parameter"] = { fg = "#B7B7B7" }
                 out["SnacksDashboardDesc"] = { fg = "#B7B7B7" }
                 out["RenderMarkdownCode"] = { fg = "#8a9a7b", bg = "#1e1e1e" }
